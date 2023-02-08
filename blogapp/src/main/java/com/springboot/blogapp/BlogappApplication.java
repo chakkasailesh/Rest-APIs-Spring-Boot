08e735473f93b2ae44119429f22a81893ad9b7b0
chakkasailesh <chakkasailesh444@gmail.com>
2023-02-08 23:59:29 +0530

    Add

---
+++ blogapp/src/main/java/com/springboot/blogapp/BlogappApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.blogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogappApplication.class, args);
+	}
+
+}
